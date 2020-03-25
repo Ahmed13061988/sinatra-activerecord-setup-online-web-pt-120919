c6faf16a500c029b11b325755ea38f874056b77f
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-25 00:27:18 +0000

    Automatically backed up by Learn

--- db/migrate/20200325002513_create_dogs.rb
+++ db/migrate/20200325002513_create_dogs.rb
@@ -1,4 +1,11 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change
+  def up 
+    create_table :dogs do |t|
+      t.string :name 
+      string :breed
+    end 
   end
+  def down
+    drob_table :dogs 
+  end 
 end
